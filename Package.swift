32f35d91e9f1c2bd30fbb99ab9724ed329b56ece
Oliver K. Ernst <oernst@ucsd.edu>
2022-01-04 21:56:54 -0800

    Updated package.swift to support swift-tools-version:5.5. Small bug fixes in iteration convergence to unwrap optionals.

--- Package.swift
+++ Package.swift
@@ -1,3 +1,4 @@
+// swift-tools-version:5.5
 /**
  This file is part of the Reductio package.
  (c) Sergio Fernández <fdz.sergio@gmail.com>
@@ -9,5 +10,29 @@
 import PackageDescription
 
 let package = Package(
-  name: "Reductio"
+    name: "Reductio",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Reductio",
+            targets: ["Reductio"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "Reductio",
+            dependencies: [],
+            path: "Source"
+        ),
+        .testTarget(
+            name: "ReductioTests",
+            dependencies: ["Reductio"],
+            path: "Tests"
+        ),
+    ]
 )
